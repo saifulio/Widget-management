84929879468d31472336faa44a27c567fcb40323
saiful <codersaif@gmail.com>
2020-11-13 18:47:26 +0100

    Initial commit

---
+++ src/main/java/com/miro/saif/task/WidgetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.miro.saif.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miro/saif/task/WidgetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.saif.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
