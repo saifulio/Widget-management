2660480f44f01df282a581422d05f2e36936b0f0
saiful <codersaif@gmail.com>
2020-11-13 19:11:28 +0100

    add single widget route

--- src/main/java/com/miro/saif/task/WidgetManagementApplication.java
+++ src/main/java/com/miro/saif/task/WidgetManagementApplication.java
@@ -2,14 +2,22 @@ package com.miro.saif.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WidgetManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WidgetManagementApplication.class, args);
 	}
 
-
+	@GetMapping("/widgets/{id}")
+	public String getWidget(@PathVariable Integer id) {
+		System.out.println(id);
+		return "Widget id: " + id;
+	}
 
 }
